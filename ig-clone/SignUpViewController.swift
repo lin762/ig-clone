3341c14a9f07a112b975a76f34cc7609e2484a21
Chris Lin <pwnzerness@gmail.com>
2018-02-04 13:15:47 -0500

    added connections to home feed view

---
+++ ig-clone/SignUpViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SignUpViewController.swift
+//  ig-clone
+//
+//  Created by Chris lin on 2/4/18.
+//  Copyright © 2018 Chris lin. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onSignUp(_ sender: Any) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ig-clone/ViewController.swift
+++ ig-clone/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func onLogin(_ sender: Any) {
+    }
+    
+    @IBAction func toSignUpPage(_ sender: Any) {
+        performSegue(withIdentifier: "signUpPageSegue", sender: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
